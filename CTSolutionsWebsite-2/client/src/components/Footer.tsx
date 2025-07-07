83efe644d14b75a75d3c32fc7bd0e8132c132eec
CTTECH108 <bastoffcial@gmail.com>
2025-07-07 16:57:47 +0530

    Footer.tsx

--- CTSolutionsWebsite-2/client/src/components/Footer.tsx
+++ CTSolutionsWebsite-2/client/src/components/Footer.tsx
@@ -93,9 +93,7 @@ export default function Footer() {
         
         <div className="border-t border-gray-700 mt-8 pt-8 text-center">
           <p className="text-gray-300 flex items-center justify-center">
-            © 2024 CTSolutions. All rights reserved. | Designed with{" "}
-            <Heart className="text-ct-primary mx-1" size={16} fill="currentColor" />
-            for the future
+            © 2025 CTSolutions. All rights reserved. | We are here for the future.
           </p>
         </div>
       </div>
